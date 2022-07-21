7e4ac782e031bd667056ed3d436b50ec18b80a00
Ishank Bansal <ishankbansal1111@gmail.com>
2022-07-21 23:45:48 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<BrowserRouter>
-    <App />
-</BrowserRouter>);
+root.render(
+    <HashRouter>
+        <App />
+    </HashRouter>);
