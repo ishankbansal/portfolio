33598d5e0ca77a0ae2541eaf4dc7af475ddcf157
Ishank Bansal <ishankbansal1111@gmail.com>
2022-07-24 21:00:31 +0530

    adding assets

---
+++ src/assets/skillsData.js
@@ -0,0 +1,27 @@
+export const skillList = [
+    {
+        skillName: 'HTML',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/html.png',
+    },
+    {
+        skillName: 'CSS',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/css.png',
+    },
+    {
+        skillName: 'JavaScript',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/js.png',
+    },
+    {
+        skillName: 'React',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/react.png',
+    },
+    {
+        skillName: 'NodeJS',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/node.png',
+    },
+    {
+        skillName: 'C++',
+        skillUrl: 'https://brandslogos.com/wp-content/uploads/images/large/c-logo.png',
+    },
+    
+];
