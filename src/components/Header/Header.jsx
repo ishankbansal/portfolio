fb36c64cff6131102e42bab9e01b81bb68f81f66
Ishank Bansal <ishankbansal1111@gmail.com>
2022-07-25 23:27:49 +0530

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
 
-const Header = ({ heading, details }) => {
+const Header = ({ heading, quoteIntro, details }) => {
     return (
         <div className='header-container'>
             <h1>{heading}</h1>
+            <h2>{quoteIntro}</h2>
             <p>{details}</p>
         </div>
     );
