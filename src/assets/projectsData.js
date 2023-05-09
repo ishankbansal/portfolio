77f72e39e4f53dc951f8cac2998f113094ee4447
Ishank Bansal <ishankbansal1111@gmail.com>
2023-05-09 20:49:52 +0530

    new project added

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -1,42 +1,35 @@
 export const projectsData = [
-    {
-        projectName: 'Memelord',
-        projectDescription:
-            'Check out this amusing WhatsApp meme bot that can deliver memes to you instantly. Just type the name of the meme and see the magic!',
-        // imageUrl: 'https://user-images.githubusercontent.com/80847853/184288782-58299dda-882b-4147-a186-e6b0cf5fb557.jpg',
-        // imageUrl: 'https://user-images.githubusercontent.com/80847853/184527985-6da39b49-4bcf-4889-97f7-bbe52a2a6857.jpg',
-        imageUrl: 'https://user-images.githubusercontent.com/80847853/184528646-a7296405-6476-4cac-b160-021c0181ee29.jpeg',
-        // imageUrl: 'https://user-images.githubusercontent.com/80847853/183925132-4ed53fb4-8a18-4f2f-b481-61b32682208e.png',
-        // imageUrl: 'https://user-images.githubusercontent.com/80847853/181936398-a709222b-6446-4942-a798-c19a43594487.png',
-        projectUrl: 'https://www.linkedin.com/feed/update/urn:li:activity:6895743471899041792/',
-    },
-    {
-        projectName: 'Buzzword Builder',
-        projectDescription:
-            'Build your own buzzy phrases within seconds. Write your own buzzwords on the picture. It is responsive with media queries and flexbox.',
-        imageUrl: 'https://user-images.githubusercontent.com/80847853/184289936-4b620b7c-a792-4ad3-a71e-cc2f3d542632.jpg',
-        projectUrl: 'https://buzzwordbuilder.netlify.app/',
-    },
-    {
-        projectName: 'Airbnb Clone',
-        projectDescription:
-            'Built the UI of Airbnb which offers unique activities. React Hooks and States were used to reuse stateful logic without changing our component hierarchy.',
-        imageUrl: 'https://user-images.githubusercontent.com/80847853/184388540-44e4bcbc-a1da-44b3-a818-fb9cc55d212e.jpg',
-        projectUrl: 'https://ishank-airbnb.netlify.app/',
-    },
-    {
-        projectName: 'Netflix Clone',
-        projectDescription:
-            "Created Netflix home page interface using HTML, CSS and Vanilla JavaScript. Used Media Queries and flexbox to make it responsive.",
-        imageUrl: 'https://user-images.githubusercontent.com/80847853/184389264-00c766f7-5652-452d-bd08-9bba92accfab.jpg',
-        projectUrl: 'https://ishank-netflixclone.netlify.app/',
-    },
-    // {
-    //     projectName: 'Portfolio Website',
-    //     projectDescription:
-    //         'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.',
-    //     imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj5.PNG',
-    //     videoUrl: 'http://theleanprogrammer.com/aam',
-    //     projectUrl: 'http://madhavbahl.tech/portfolio/',
-    // },
+  {
+    projectName: "Memelord",
+    projectDescription:
+      "Check out this amusing WhatsApp meme bot that can deliver memes to you instantly. Just type the name of the meme and see the magic!",
+    imageUrl:
+      "https://user-images.githubusercontent.com/80847853/184528646-a7296405-6476-4cac-b160-021c0181ee29.jpeg",
+    projectUrl:
+      "https://www.linkedin.com/feed/update/urn:li:activity:6895743471899041792/",
+  },
+  {
+    projectName: "samवाद",
+    projectDescription:
+      "Introducing samवाद - वाद privately 💬. A real-time chat application that lets you converse with any registered user.",
+    imageUrl:
+      "https://user-images.githubusercontent.com/80847853/229187887-d1515477-f7e3-4ebe-a6f8-c995bb738ad1.png",
+    projectUrl: "https://github.com/ishankbansal/samvaad",
+  },
+  {
+    projectName: "Buzzword Builder",
+    projectDescription:
+      "Build your own buzzy phrases within seconds. Write your own buzzwords on the picture. It is responsive with media queries and flexbox.",
+    imageUrl:
+      "https://user-images.githubusercontent.com/80847853/184289936-4b620b7c-a792-4ad3-a71e-cc2f3d542632.jpg",
+    projectUrl: "https://buzzwordbuilder.netlify.app/",
+  },
+  {
+    projectName: "Netflix Clone",
+    projectDescription:
+      "Created Netflix home page interface using HTML, CSS and Vanilla JavaScript. Used Media Queries and flexbox to make it responsive.",
+    imageUrl:
+      "https://user-images.githubusercontent.com/80847853/184389264-00c766f7-5652-452d-bd08-9bba92accfab.jpg",
+    projectUrl: "https://ishank-netflixclone.netlify.app/",
+  },
 ];
