9fb73e48ed173bd8a5fc368a14496d4380abfc6a
Ishank Bansal <ishankbansal1111@gmail.com>
2023-05-09 20:50:09 +0530

    new skill added

--- src/assets/skillsData.js
+++ src/assets/skillsData.js
@@ -1,28 +1,42 @@
 // import cppImage from './cpp.png'
 export const skillList = [
-    {
-        skillName: 'C++',
-        skillUrl: '../../assets/cpp.png',
-    },
-    {
-        skillName: 'HTML',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/html.png',
-    },
-    {
-        skillName: 'CSS',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/css.png',
-    },
-    {
-        skillName: 'JavaScript',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/js.png',
-    },
-    {
-        skillName: 'React',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/react.png',
-    },
-    {
-        skillName: 'NodeJS',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/node.png',
-    },
-    
+  {
+    skillName: "C++",
+    skillUrl: "../../assets/cpp.png",
+  },
+  {
+    skillName: "HTML",
+    skillUrl:
+      "https://user-images.githubusercontent.com/80847853/230729623-d14b54c2-179f-4d44-b5c0-4a47e73b398b.jpeg",
+  },
+  {
+    skillName: "CSS",
+    skillUrl:
+      "https://user-images.githubusercontent.com/80847853/230729666-a9d4c669-44f3-4c4b-bf7c-6d3dd9aa2bc0.jpeg",
+  },
+  {
+    skillName: "JavaScript",
+    skillUrl:
+      "https://user-images.githubusercontent.com/80847853/230729678-0cb0b6d8-f495-4146-91d9-b84589b57286.jpeg",
+  },
+  {
+    skillName: "React",
+    skillUrl:
+      "https://user-images.githubusercontent.com/80847853/230729698-75b6757d-421a-4718-9d7a-d2b648106de5.jpeg",
+  },
+  {
+    skillName: "NodeJS",
+    skillUrl:
+      "https://user-images.githubusercontent.com/80847853/230729717-76b3e891-5beb-4f96-8b88-3bc7a939907e.jpeg",
+  },
+  {
+    skillName: "Firebase",
+    skillUrl:
+      "https://user-images.githubusercontent.com/80847853/230728399-d59b2348-37f9-41c0-9853-a42f1801c120.png",
+  },
+  {
+    skillName: "SASS",
+    skillUrl:
+      "https://user-images.githubusercontent.com/80847853/230728524-a16c026e-6679-4731-8926-2504db9a46fa.png",
+  },
 ];
